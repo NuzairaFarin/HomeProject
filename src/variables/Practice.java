f493bbe1f812df272461bb36cba05107b3c6fe81
Nuzaira Farin <nuzairafarin@nuzairas-mbp.myfiosgateway.com>
2022-11-02 21:13:41 -0400

    9.13 pm

--- src/variables/Practice.java
+++ src/variables/Practice.java
@@ -5,8 +5,7 @@ public class Practice {
 	public static void main(String[] args) {
 		String n= "100";
 System.out.println(n+" "+(20+10));
-System.out.println("hi");
-System.out.println("howdy");
+//System.out.println("hi");
 	}
 
 }
